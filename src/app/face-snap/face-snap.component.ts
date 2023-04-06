aef47c94b21a2eb68a15fb10cefa23d21073dfe0
Jarvis182 <patrice.hanne@gmail.com>
2023-04-06 16:28:43 +0200

    [Angular] 001 - Etat initial : Oh Snap!

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  buttonText!: string;
+
+
+  ngOnInit() {
+    this.title = 'Coucher de soleil';
+    this.description = 'Mon meilleur ami depuis tout petit !';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg'
+    this.buttonText = "Oh Snap!";
+  }
+
+  onSnap(){
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
+  }
+}
