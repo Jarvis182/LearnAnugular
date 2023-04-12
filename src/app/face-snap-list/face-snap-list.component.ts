5940e6e64974068043b9d5230a20ea9be0e09580
Jarvis182 <patrice.hanne@gmail.com>
2023-04-12 18:56:44 +0200

    [ANGULAR] 006 - FaceSnapList Constructor en place

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snaps.servcice';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -9,54 +10,9 @@ import { FaceSnap } from '../models/face-snap.model';
 export class FaceSnapListComponent implements OnInit {
   faceSnaps!: FaceSnap[];
 
+  constructor(private faceSnapsService: FaceSnapsService) { }
+
   ngOnInit() {
-      this.faceSnaps = [
-        {
-          title: 'Forest',
-          description: 'C\'est beau non? ',
-          imageUrl: 'https://i.vimeocdn.com/video/1401922789-d1f5ab67f81f72ad61a942eda022df97ffb94ff9de856c13fc12936de0f1375f-d_640',
-          createdDate: new Date(),
-          snaps: 10,
-          location: 'Madrid'
-    
-        },
-        {
-          title: 'MotoCross',
-          description: 'Je vole ! ',
-          imageUrl: 'https://cdn.pixabay.com/photo/2014/07/05/16/44/biker-384921_960_720.jpg',
-          createdDate: new Date(),
-          snaps: 50,
-          location: 'Paris'
-        },
-        {
-          title: 'harrier',
-          description:  'Trop la classe ',
-          imageUrl: 'https://cdn.pixabay.com/photo/2017/07/19/00/32/av-8b-harrier-2517552_960_720.jpg',
-          createdDate: new Date(),
-          snaps: 99,
-          location: 'Toulouse'
-        },
-        {
-          title: 'BMW',
-          description: 'Drift ? ',
-          imageUrl: 'https://cdn.pixabay.com/photo/2021/04/29/08/47/bmw-m3-6216130_960_720.jpg',
-          createdDate: new Date(),
-          snaps: 200
-        },
-        {
-          title: 'Z1000',
-          description: 'le bonheur :)',
-          imageUrl:  'https://cdn.pixabay.com/photo/2020/11/12/14/22/motorcycle-5735642_960_720.jpg',
-          createdDate: new Date(),
-          snaps: 4000
-        },
-        {
-          title: 'RS6',
-          description: 'DreamCar',
-          imageUrl:  'https://cdn.pixabay.com/photo/2018/04/23/08/00/car-3343430_960_720.jpg',
-          createdDate: new Date(),
-          snaps: 199
-        }
-      ];
+    this.faceSnaps = this.faceSnapsService.faceSnaps;
   }
 }
